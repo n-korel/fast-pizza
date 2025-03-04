fb32c4b9ef2e77fbe29ef429fcf43857ca78791f
n-korel <n.korelskiy@hotmail.com>
2025-03-04 17:59:22 +0300

    Data loading

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import prettier from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
     { ignores: ['dist'] },
@@ -16,13 +17,24 @@ export default [
                 sourceType: 'module',
             },
         },
+        settings: { react: { version: '18.3' } },
         plugins: {
-            prettier, // Подключаем плагин Prettier
+            react,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            prettier,
         },
         rules: {
             ...js.configs.recommended.rules,
-            ...prettierConfig.rules,
-            'prettier/prettier': 'error',
+            ...react.configs.recommended.rules,
+            ...react.configs['jsx-runtime'].rules,
+            ...reactHooks.configs.recommended.rules,
+            'react/jsx-no-target-blank': 'off',
+            'react/prop-types': 'off',
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
         },
     },
 ];
