07eea02ca04c24996a15c1b6b2ec9077306120f7
n-korel <n.korelskiy@hotmail.com>
2025-03-07 17:37:54 +0300

    Cart Overview

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -33,6 +34,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+    const username = useSelector((state) => state.user.username);
     const navigation = useNavigation();
     const isSubmitting = navigation.state === 'submitting';
 
@@ -56,6 +58,7 @@ function CreateOrder() {
                         name="customer"
                         required
                         className="input grow"
+                        defaultValue={username}
                     />
                 </div>
 
